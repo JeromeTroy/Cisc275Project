29f50dbc23c77582cefab707e1868e5caadef585
JeromeTroy <jrtroy@udel.edu>
2017-10-04 11:49:04 -0400

    Implemented getters and setters

--- src/model/MainMap.java
+++ src/model/MainMap.java
@@ -5,4 +5,35 @@ public class MainMap {
 	private int length; 			// total length of the map
 	private int height; 			// total height of the map
 	private int uniqueLength; 		// length of the map that is unique, beyond this will be periodic
+	
+	public int getLength(){
+		return length;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+	
+	public MainMap(int l, int h, int ul){
+		length = l;
+		height = h;
+		uniqueLength = ul;
+	}
+	
+	public MainMap(int l, int h){
+		length = l;
+		height = h;
+	}
+	
+	public void setLength(int l){
+		length = l;
+	}
+	
+	public void setHeight(int h){
+		height = h;
+	}
+	
+	public void setUniqueLength(int u){
+		uniqueLength = u;
+	}
 }
