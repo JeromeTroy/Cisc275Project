bc856fe18e80bc84f9c15f1f851ea7d022a5fc6d
alanijohnson <alanijjohnson@gmail.com>
2017-10-27 19:42:48 -0400

    Updated StuffSet add() to add if no collision add method sorts using comparator after each add add method checks to ensure fish isn't colliding StuffSet no longer contains DistToFishConstructor

--- src/model/StuffSet.java
+++ src/model/StuffSet.java
@@ -2,24 +2,24 @@ package model;
 
 import java.util.*;
 
-public class StuffSet extends ArrayList<StuffInOcean> implements Comparable<StuffInOcean> {
-	//sort method update to sort by distance
-	//compareTo for adding 
-	//add elements off screen 45 degree angle arc
+public class StuffSet extends ArrayList<StuffInOcean> {
+	// sort method update to sort by distance
+	// compareTo for adding
+	// add elements off screen 45 degree angle arc
 	// -- pull screen size from controller
 	// --fix to center of corner
-	
+
 	int partitionDist;
 	FishCharacter f;
-	
+
 	public StuffSet() {
 	}
 
 	public StuffSet(FishCharacter f) {
 		this.f = f;
-		add(f);
+		super.add(f);
 	}
-	
+
 	public StuffSet(FishCharacter f, int partitionDist) {
 		this(f);
 		this.partitionDist = partitionDist;
@@ -35,34 +35,39 @@ public class StuffSet extends ArrayList<StuffInOcean> implements Comparable<Stuf
 	 * addition was successful
 	 */
 	public boolean add(StuffInOcean s) {
-		
-		
-		
-		
-		
+
+		for (StuffInOcean item : this) {
+			if (s.isCollided(item)) {
+				return false;
+			}
+		}
+
 		super.add(s);
-		Collections.sort(this);
-		int currIndex = indexOf(s);
-		boolean goodAdd = true;
+		Collections.sort(this, new DistToFishComparator(f));
+		return true;
 
-		try {
-			StuffInOcean ahead = (StuffInOcean) get(currIndex - 1);
-			goodAdd = !s.isCollided(ahead);
-		} catch (IndexOutOfBoundsException e) {
-			goodAdd = true;
-		}
-
-		try {
-			StuffInOcean behind = (StuffInOcean) get(currIndex + 1);
-			goodAdd = !s.isCollided(behind);
-		} catch (IndexOutOfBoundsException e) {
-		}
-
-		if (!goodAdd) {
-			remove(s);
-		}
-		return goodAdd;
-
+		// super.add(s);
+		// Collections.sort(this, new DistToFishComparator(f));
+		// int currIndex = indexOf(s);
+		// boolean goodAdd = true;
+		//
+		// try {
+		// StuffInOcean ahead = (StuffInOcean) get(currIndex - 1);
+		// goodAdd = !s.isCollided(ahead);
+		// } catch (IndexOutOfBoundsException e) {
+		// goodAdd = true;
+		// }
+		//
+		// try {
+		// StuffInOcean behind = (StuffInOcean) get(currIndex + 1);
+		// goodAdd = !s.isCollided(behind);
+		// } catch (IndexOutOfBoundsException e) {
+		// }
+		//
+		// if (!goodAdd) {
+		// remove(s);
+		// }
+		// return goodAdd;
 	}
 
 }
