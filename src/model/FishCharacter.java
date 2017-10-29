1512d189bcc92b7f37c94e1a6516d902e77c2c42
JeromeTroy <jrtroy@udel.edu>
2017-10-29 17:45:09 -0400

    Fixing Fish Character so it matches master before merge

--- src/model/FishCharacter.java
+++ src/model/FishCharacter.java
@@ -3,23 +3,12 @@ package model;
 import java.util.*;
 import java.math.*;
 public class FishCharacter extends StuffInOcean{
+	protected int radius = 1;
+	// Attributes //TODO: remove comments
 	
-	// Attributes
-	
-	/*
-	private int xIncr = 1;		// x increment when moving
-	private int yIncr = 1;		// y increment when moving
-	*/
 	private int step = 1;
 	
 	// orientations
-	/*
-	private boolean north;				// is the fish facing north
-	private boolean south;				// is the fish facing south
-	private boolean east; 				// is the fish facing east
-	private boolean west; 				// is the fish facing west
-	*/
-	
 	private int angle; 					// angle (counterclockwise) from east facing
 	
 	private int score;			// player's score
@@ -40,18 +29,12 @@ public class FishCharacter extends StuffInOcean{
 	 */
 	public FishCharacter(){	// TODO implement view parameters through controller
 		
-		position = new OurVector(5,5);
-		radius = step;
+		position = new Vector(10,10); //TODO: changed initial location from 5,5 to 10,10
+		//radius = step;
 		score = 0; 			// set score
 		
 		// set orientation
-		/*
-		east = true;
-		west = false;
-		north = false;
-		south = false;
 		isCaught = false;
-		*/
 		angle = 0;
 		possibleOrientations.add("north");
 		possibleOrientations.add("northwest");
@@ -72,25 +55,37 @@ public class FishCharacter extends StuffInOcean{
 	 * 		None
 	 * moves the fish
 	 */
-	public void move() {
+	public void move(Map m) {
 		// moves the fish	
-		/*
-		if (north) {
-			position.setY(position.getY() - yIncr);
-		} else if (south) {
-			position.setY(position.getY() + yIncr);
-		}
-		if (east) {
-			position.setX(position.getX() + xIncr);
-		} else if (west) {
-			position.setX(position.getX() - xIncr);
-		}
-		*/
+		
+		// temporary storage
+		int prevX = position.getX();
+		int prevY = position.getY();
+		
+		// moving
 		int deltaX = (int) Math.cos(Math.toRadians(angle))*step;
 		int deltaY = (int) Math.sin(Math.toRadians(angle))*step;
 		position.setX(position.getX() + deltaX);
 		position.setY(position.getY() + deltaY);
+		
+		// bounds handling
+		if ((position.getX() > m.getLength()) || (position.getX() < 0)){
+			position.setX(prevX);
+		}
+		if ((position.getY() > m.getHeight()) || (position.getY() < 0)){
+			position.setY(prevY);
+		}
 	}
+	
+	public String getName(){
+		return "Fish ";
+	}
+	
+	public boolean isFish(){
+		return true;
+	}
+	
+	
 	
 	/*
 	 * Rotations of the fish
@@ -100,33 +95,6 @@ public class FishCharacter extends StuffInOcean{
 	 * 		None
 	 */
 	public void rotate(int degrees){
-		/*
-		int currIndex = possibleOrientations.indexOf(getOrientation());
-		String newOrientation = "";
-		if (degrees < 45){
-			newOrientation = getOrientation();
-			extractOrientationFromString(newOrientation);
-		}else if ((degrees > 45) && (degrees < 90)){
-			newOrientation = possibleOrientations.get(currIndex + 1);
-			extractOrientationFromString(newOrientation);
-		}else if ((degrees > 90) && (degrees < 135)){
-			newOrientation = possibleOrientations.get(currIndex + 2);
-			extractOrientationFromString(newOrientation);
-		}else if ((degrees > 135) && (degrees < 180)){
-			newOrientation = possibleOrientations.get(currIndex + 3);
-			extractOrientationFromString(newOrientation);
-		}else if (degrees > 180){
-			north = !north;
-			south = !south;
-			east = !east;
-			west = !west;
-			rotate(degrees - 180);
-		}else if (degrees < 0){
-			rotate(degrees + 360);
-		}else{
-			System.out.println("invalid degrees, do not know how to handle, degrees = " + degrees);
-		}
-		*/
 		angle += degrees;
 		if (angle < 0){
 			rotate(360);
@@ -134,22 +102,6 @@ public class FishCharacter extends StuffInOcean{
 			rotate(-360);
 		}
 	}
-	
-	/*
-	public void extractOrientationFromString(String or){
-		north = or.startsWith("north");
-		south = or.startsWith("south");
-		east = or.endsWith("east");
-		west = or.endsWith("west");
-		if (north && south){ 
-			north = false;
-			south = false;
-		}
-		if (east && west){
-			west = false;
-		}
-	}
-	*/
 	
 	// TODO verify this implementation of contact and getting caught
 	
@@ -160,20 +112,6 @@ public class FishCharacter extends StuffInOcean{
 	// TODO bounds handling
 	
 	public String getOrientation(){
-		/*
-		String orient = "";
-		if (north) {
-			orient += "north";
-		}else if (south) {
-			orient += "south";
-		}
-		if (east) {
-			orient += "east";
-		}else if (west) {
-			orient += "west";
-		}
-		return orient;
-		*/
 		String orient = "";
 		if ((angle >= 45/2) && (angle <= (180-45/2))){
 			orient += "north";
@@ -200,8 +138,17 @@ public class FishCharacter extends StuffInOcean{
 		return score;
 	}
 	
+	public int getRadius(){
+		return radius;
+	}
+	
 	// setters
 	public void setStepSize(int l){
 		step = l;
 	}
+	
+	public void setRadius(int r){
+		radius = r;
+	}
+	
 }
