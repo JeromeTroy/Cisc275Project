1acd87aedddd98972d8696010fe2863b9d90ca2a
alanijohnson <alanijjohnson@gmail.com>
2017-10-29 20:48:56 -0400

    Merged MainGameThread & GameTimer

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 //TODO: organize imports
 import model.*;
 import javax.swing.*;
@@ -9,81 +8,80 @@ import java.util.*;
 import java.util.Timer;
 
 public class MainController {
-	private static GameTimer timer;
+	// private static GameTimer timer;
 	public static MainGameModel mainGameModel;
 	private static MainGameThread mainGameThread;
 	public static MiniGameModel miniGame;
-	private static int tickPeriod = 30; //in milliseconds
-	
-	public MainController(){
+	private static int tickPeriod = 30; // in milliseconds
+
+	public MainController() {
 		mainGameModel = new MainGameModel();
-		timer = new GameTimer(mainGameModel.getGameLengthSeconds());
-		mainGameThread = new MainGameThread(tickPeriod);
-		
-		}
-	
-	
+		// timer = new GameTimer(mainGameModel.getGameLengthSeconds(),
+		// tickPeriod);
+		mainGameThread = new MainGameThread(mainGameModel.getGameLengthSeconds(), tickPeriod);
+
+	}
+
 	public static void main(String[] args) {
 		openGame();
 	}
 
-	public MainGameModel getModel(){
+	public MainGameModel getModel() {
 		return mainGameModel;
 	}
-	
-	public static void openGame(){
+
+	public static void openGame() {
 		Runnable theGame = new RunGame();
 		javax.swing.SwingUtilities.invokeLater(theGame);
 	}
-	
-	public static void startGame(){
+
+	public static void startGame() {
 		MainController controller = new MainController();
 		mainGameThread.start();
-		
-	
-}
-	public static void startTutorial(){
+
+	}
+
+	public static void startTutorial() {
 		System.out.println("Start Tutorial");
 	}
-	
+
 	protected static void tick() {
 		System.out.println("Tick");
-		
-		if (mainGameThread.inMiniGame){
+
+		if (mainGameThread.inMiniGame) {
 			miniGame.update();
-			if (miniGame.isGameOver()){
+			if (miniGame.isGameOver()) {
 				endMiniGame();
 			}
-		} else{
+		} else {
 			mainGameModel.update();
-			if (mainGameModel.isGameOver()){
-				endGame();
-			}
 		}
-		if (mainGameModel.getIsCaught()){
+		if (mainGameModel.isGameOver()) {
+			endGame();
+		}
+		if (mainGameModel.getIsCaught()) {
 			launchMiniGame();
 		}
-		
+
 	}
-	
-	public static void endGame(){
-		//TODO: This part isn't working as expected
+
+	public static void endGame() {
+		// TODO: This part isn't working as expected
 		mainGameThread.stopTick();
 		System.out.println("Game Over");
 		System.out.println("End Screen");
 	}
-	
-	public static void launchMiniGame(){
+
+	public static void launchMiniGame() {
 		mainGameThread.enterMiniGameMode();
 		miniGame = new MiniGameModel();
 		System.out.println("MiniGame Launched...");
 		mainGameModel.setCaught(false);
-		
+
 	}
-	
-	public static void endMiniGame(){
+
+	public static void endMiniGame() {
 		mainGameThread.exitMiniGameMode();
 	}
-	
-	
+
 }
--- src/controller/MainGameThread.java
+++ src/controller/MainGameThread.java
@@ -2,18 +2,22 @@ package controller;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
+import java.util.concurrent.TimeUnit;
 import model.*;
 
 public class MainGameThread extends Thread implements Runnable {
 	Timer timer;
-	boolean inMiniGame; //is the game tick being paused here? The timer displayed is independent of the game timer
+	boolean inMiniGame; // is the game tick being paused here? The timer
+						// displayed is independent of the game timer
 	int tickPeriod;
-	
-	public MainGameThread(int tickPeriod) {
+	int gameLength; // in milliseconds
+	int currentTimeElapsed = 0; // in milliseconds
+
+	public MainGameThread(int gameLengthInSeconds, int tickPeriod) {
 		// this.model = model;
 		this.timer = new Timer();
 		this.tickPeriod = tickPeriod;
+		this.gameLength = gameLengthInSeconds * 1000;
 	}
 
 	@Override
@@ -21,23 +25,33 @@ public class MainGameThread extends Thread implements Runnable {
 		System.out.println("Main Game Run Begin");
 		timer.scheduleAtFixedRate(new TimerTask() {
 			public void run() {
-				
-					MainController.tick();
-				
+				currentTimeElapsed += tickPeriod;
+				MainController.tick();
+
 			}
 		}, 0, tickPeriod);
 
 	}
-	
-	public void enterMiniGameMode(){
+
+	public void enterMiniGameMode() {
 		inMiniGame = true;
 	}
-	
-	public void exitMiniGameMode(){
+
+	public void exitMiniGameMode() {
 		inMiniGame = false;
 	}
-	
-	public void stopTick(){
+
+	public void stopTick() {
 		timer.cancel();
+	}
+
+	@Override
+	public String toString() {
+		// String.format("%2d min, %02d sec",
+		// TimeUnit.MILLISECONDS.toMinutes(millis),
+		// TimeUnit.MILLISECONDS.toSeconds(millis) -
+		// TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))
+		// );
+		return "time"; // TODO: return actual time
 	}
 }
