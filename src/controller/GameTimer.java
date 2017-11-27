8cecdaaf7646de4182bf9c6c090f431db26f7729
alanijohnson <alanijjohnson@gmail.com>
2017-11-27 17:24:21 -0500

    Update GameTimer to be an action fire

--- src/controller/GameTimer.java
+++ src/controller/GameTimer.java
@@ -9,24 +9,15 @@ import javax.swing.Timer;
 public class GameTimer extends Thread implements Runnable {
 	Timer timer; 
 	private int tickPeriod; //in milliseconds
-	private int gameLength; // in milliseconds
-	private int timeElapsed = 0; // in milliseconds
-	//private MainController c;  
-	//TODO: implement miniGame timer
+	MainController c;
 	
 	public GameTimer(MainController c) {
+		this.c = c;
 		this.tickPeriod = c.getTickPeriod();
-		this.gameLength = c.getGameLength();
 		
 		//create Swing timer with actionListener
-		this.timer = new Timer(tickPeriod, new ActionListener(){
-		    public void actionPerformed(ActionEvent e) {
-		        c.tick();
-		        //c.getGamePlayScreen().updateFishPosition();
-		        }
-		});
+		this.timer = createTimer(tickPeriod);
 		
-		timeElapsed+=tickPeriod;
 		
 	}
 	
@@ -37,30 +28,31 @@ public class GameTimer extends Thread implements Runnable {
 	
 	//stop the timer
 	public void stopTimer(){
-		try {
-			timer.wait();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}	
+		timer.stop();	
+	}
+	
+	public void pauseTimer(){
+		timer.stop();
+		timer = createTimer(tickPeriod);
+	}
+	
 	
 	//get timer
 	public Timer getSwingTimer(){
 		return timer;
 	}
-
 	
-	/* toString() - returns the remaining time in the game in min:second format
-	 * (non-Javadoc)
-	 * @see java.lang.Thread#toString()
-	 */
-	@Override
-	public String toString() {
-		int millis = gameLength - timeElapsed;
-		return String.format("%2d min, %02d sec", TimeUnit.MILLISECONDS.toMinutes(millis), TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
-		//return  // TODO: return actual time
+	//create timer
+	public Timer createTimer(int tickPeriod){
+		return new Timer(tickPeriod, new ActionListener(){
+		    public void actionPerformed(ActionEvent e) {
+		        c.tick();
+		        }
+	});
 	}
+	
+	
+
 	
 }
 
