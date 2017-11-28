910607e990810b980b91eae6c2f096d9e820cc37
mcollins12321 <mcollins12321@gmail.com>
2017-11-27 21:33:43 -0500

    centered mini-game

--- src/view/TutorialScreen.java
+++ src/view/TutorialScreen.java
@@ -34,7 +34,7 @@ import javax.swing.JButton;
  * @author Team4
  *
  */
-public class TutorialScreen extends GodView {
+public class TutorialScreen extends JPanel implements ActionListener {
 
 	// Swing components
 	private static JLabel instructions;
@@ -137,6 +137,8 @@ public class TutorialScreen extends GodView {
 
 		// add game panel
 		gamePanel = new PlayScreen();
+		gamePanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
 		gamePanel.setPreferredSize(new Dimension(playLength, playHeight));
 		gamePanel.setBorder(BorderFactory.createLineBorder(Color.red));
 
@@ -146,7 +148,8 @@ public class TutorialScreen extends GodView {
 		mgs = new MiniGameScreen(gamePanel.getWidth() / 2, gamePanel.getHeight() / 2, playLength / 2, playHeight / 2);
 		layeredPane.add(mgs);
 		layeredPane.setPreferredSize(new Dimension(playLength / 2, playHeight / 2)); // resize
-		gamePanel.add(layeredPane, new Integer(300));
+		gamePanel.add(layeredPane, gbc);
+		gamePanel.revalidate();
 		add(gamePanel);
 		
 		//mgs.setVisible(true);
@@ -314,7 +317,7 @@ public class TutorialScreen extends GodView {
 			if (dispFood) {
 				for (int i = 0; i < numFood; i++) {
 					foodxLocation[i] -= autoscrolldpt;
-					g.drawImage(foodImage, foodxLocation[i]-(foodImage.getWidth()/2), foodyLocation[i]-(foodImage.getHeight()/2), this);
+					g.drawImage(foodImage, foodxLocation[i], foodyLocation[i] + 50, this);
 					// System.out.println((foodxLocation[i]-j)+" "+
 					// foodyLocation[i]);
 					// g.drawImage(foodImage, 1000, 250, this);
@@ -323,12 +326,12 @@ public class TutorialScreen extends GodView {
 			if (dispTrash) {
 				for (int i = 0; i < numTrash; i++) {
 					trashxLocation[i] -= autoscrolldpt;
-					g.drawImage(trashImage, trashxLocation[i]-(trashImage.getWidth()/2), trashyLocation[i]-(foodImage.getHeight()/2), this);
+					g.drawImage(trashImage, trashxLocation[i], trashyLocation[i] + 50, this);
 				}
 			}
 
 			// disp fish
-			g.drawImage(fishImage, cursorx-(fishImage.getWidth()/2), cursory-(fishImage.getHeight()/2), this);
+			g.drawImage(fishImage, cursorx, cursory, this);
 			//g2d.drawImage(fishImage, fishxLocation, fishyLocation, this);
 			// display instructions
 			// TODO: add game intruction tutorial mode
